5d3a1205b5733c25ff6d78b1b8c0763a6272626e
kyr0 <info@aron-homberg.de>
2023-02-12 21:30:00 +0100

    feat: add --dev flag to control debug build mode

--- build.mjs
+++ build.mjs
@@ -3,6 +3,7 @@ import { buildForNode } from '@jsheaven/easybuild'
 await buildForNode({
   entryPoint: './src/index.ts',
   outfile: './dist/index.js',
+  debug: process.argv.indexOf('--dev') > -1,
   esBuildOptions: {
     logLevel: 'error',
   },
@@ -11,6 +12,7 @@ await buildForNode({
 await buildForNode({
   entryPoint: './src/cli.ts',
   outfile: './dist/cli.js',
+  debug: process.argv.indexOf('--dev') > -1,
   esBuildOptions: {
     bundle: true,
     logLevel: 'error',
